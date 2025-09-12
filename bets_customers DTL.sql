4b9101d3bc153db9f363450750d7d0f11ba37a8a
Lazo2223 <d.lazarov2223@gmail.com>
2025-09-12 13:29:29 +0300

    Update bets_customers DTL.sql

--- bets_customers DTL.sql
+++ bets_customers DTL.sql
@@ -37,3 +37,36 @@ FROM STREAM read_files(
 --	•	DLT tables get their schema either:
 --	1.	Inferred from the source (read_files), or
 --	2.	Explicitly given via schema => "..." inside read_files.
+
+
+
+---Silver and some CASE and CAST functions
+
+CREATE OR REFRESH STREAMING TABLE mimic_gaming_data.customers_silver.customers_in_silver
+AS
+SELECT
+  customer_id,
+  username,
+  first_name,
+  last_name,
+  TRY_CAST(date_of_birth AS DATE) AS date_of_birth,
+  CASE
+  WHEN date_of_birth IS NULL THEN NULL
+  WHEN date_of_birth < '1939-01-01' THEN "Old Player"
+  WHEN date_of_birth > '2000-01-01' THEN "Young Player"
+  ELSE date_of_birth
+  END AS date_of_birth_checked,
+  place_of_birth,
+  CASE
+  WHEN place_of_birth = 'United Kingdom' THEN "NonEU"
+  ELSE place_of_birth
+  END AS is_UK_citizen,
+  country,
+  state_province,
+  TRY_CAST(signup_date AS DATE) AS signup_date,
+  vip_status,
+  TRY_CAST(self_excluded AS BOOLEAN) AS self_excluded,
+  preferred_channel,
+  ingest_timestamp,
+  file_name
+FROM STREAM mimic_gaming_data.customers_bronze.customers;
